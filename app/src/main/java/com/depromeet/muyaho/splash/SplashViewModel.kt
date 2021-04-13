89813fa49116ca1d82287affaea454da0818e2af
mjjang <inseok16@gmail.com>
2021-04-14 01:13:49 +0900

    fix error

--- app/src/main/java/com/depromeet/muyaho/splash/SplashViewModel.kt
+++ app/src/main/java/com/depromeet/muyaho/splash/SplashViewModel.kt
@@ -2,9 +2,10 @@ package com.depromeet.muyaho.splash
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel: ViewModel() {
+class SplashViewModel @Inject constructor(): ViewModel() {
     init {
         // 초기화 작업은 여기에서 진행
     }
