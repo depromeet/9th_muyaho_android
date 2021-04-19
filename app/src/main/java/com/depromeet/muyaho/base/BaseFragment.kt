3cd41c29451a4381b7d7c1457a3161b7b6c3e0a8
SieunSong <se.song@riiid.co>
2021-04-19 23:37:20 +0900

    Update BaseFragment.kt

--- app/src/main/java/com/depromeet/muyaho/base/BaseFragment.kt
+++ app/src/main/java/com/depromeet/muyaho/base/BaseFragment.kt
@@ -1,8 +1,34 @@
 package com.depromeet.muyaho.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
 
-class BaseFragment : Fragment() {
-    // DataBinding
+abstract class BaseFragment<T : ViewDataBinding, R : BaseViewModel<A>, A : Action> : Fragment() {
+    protected lateinit var binding: T
+
+    abstract val layoutResId: Int
+    abstract val vm: R
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (vm !is NoViewModel) binding.setVariable(BR.vm, vm)
+    }
 
 }
