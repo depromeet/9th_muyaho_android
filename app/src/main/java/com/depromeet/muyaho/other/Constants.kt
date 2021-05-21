d590d9cfa91a85a4ce4238918f5e92f4f144d6a8
SieunSong <se.song@riiid.co>
2021-05-22 00:47:25 +0900

    code 상수로 정의

--- app/src/main/java/com/depromeet/muyaho/other/Constants.kt
+++ app/src/main/java/com/depromeet/muyaho/other/Constants.kt
@@ -2,4 +2,8 @@ package com.depromeet.muyaho.other
 
 object Constants {
     const val BASE_URL = "http://muyaho-api-alb-302156047.ap-northeast-2.elb.amazonaws.com"
+    const val CODE_200_OK = 200
+    const val CODE_400_BAD_REQUEST = 400
+    const val CODE_404_NOT_FOUND = 404
+    const val CODE_409_CONFLICT = 409
 }
