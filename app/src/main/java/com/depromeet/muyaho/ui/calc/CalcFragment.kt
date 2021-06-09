51cd259fe4d6191651f0b029d90ed29db9ee4566
SieunSong <se.song@riiid.co>
2021-06-09 23:39:31 +0900

    수익률 탭 ui 구현

--- app/src/main/java/com/depromeet/muyaho/ui/calc/CalcFragment.kt
+++ app/src/main/java/com/depromeet/muyaho/ui/calc/CalcFragment.kt
@@ -1,13 +1,14 @@
 package com.depromeet.muyaho.ui.calc
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.depromeet.muyaho.R
 import com.depromeet.muyaho.base.BaseFragment
 import com.depromeet.muyaho.databinding.FragmentCalcBinding
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class CalcFragment : BaseFragment<FragmentCalcBinding, CalcViewModel, CalcViewModel.ViewAction>() {
@@ -15,10 +16,16 @@ class CalcFragment : BaseFragment<FragmentCalcBinding, CalcViewModel, CalcViewMo
     override val layoutResId: Int = R.layout.fragment_calc
     override val vm: CalcViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenResumed { vm.init() }
+    }
+
     override fun observeActionCommand(action: CalcViewModel.ViewAction) {
         when (action) {
             CalcViewModel.ViewAction.ShowHistory -> navigate(HistoryFragment.newInstance())
-            CalcViewModel.ViewAction.ShowRevenueRate -> navigate(RevenueRateFragment.newInstance())
+            CalcViewModel.ViewAction.ShowIncomeRate -> navigate(IncomeRateFragment.newInstance())
             CalcViewModel.ViewAction.ShowWater -> navigate(WaterFragment.newInstance())
         }
     }
@@ -27,7 +34,6 @@ class CalcFragment : BaseFragment<FragmentCalcBinding, CalcViewModel, CalcViewMo
         val childFt: FragmentTransaction = childFragmentManager.beginTransaction()
         if (!child.isAdded) {
             childFt.replace(R.id.fragment_container, child)
-            childFt.addToBackStack(null)
             childFt.commit()
         }
     }
--- app/src/main/java/com/depromeet/muyaho/ui/calc/CalcViewModel.kt
+++ app/src/main/java/com/depromeet/muyaho/ui/calc/CalcViewModel.kt
@@ -1,5 +1,7 @@
 package com.depromeet.muyaho.ui.calc
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.depromeet.muyaho.base.Action
 import com.depromeet.muyaho.base.BaseViewModel
@@ -13,25 +15,40 @@ class CalcViewModel @Inject constructor(
 ) : BaseViewModel<CalcViewModel.ViewAction>() {
     sealed class ViewAction : Action {
         object ShowHistory : ViewAction()
-        object ShowRevenueRate : ViewAction()
+        object ShowIncomeRate : ViewAction()
         object ShowWater : ViewAction()
     }
 
-    init {
-        viewModelScope.launch {
-            actionSender.send(ViewAction.ShowHistory)
-        }
+    data class State(
+        val currentTab: Tab = Tab.HISTORY
+    )
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+    fun init() {
+        onClickHistory()
     }
 
     fun onClickHistory() = viewModelScope.launch {
         actionSender.send(ViewAction.ShowHistory)
+        _state.value = State(currentTab = Tab.HISTORY)
     }
 
-    fun onClickRevenueRate() = viewModelScope.launch {
-        actionSender.send(ViewAction.ShowRevenueRate)
+    fun onClickIncomeRate() = viewModelScope.launch {
+        actionSender.send(ViewAction.ShowIncomeRate)
+        _state.value = State(currentTab = Tab.INCOME_RATE)
     }
 
     fun onClickWater() = viewModelScope.launch {
         actionSender.send(ViewAction.ShowWater)
+        _state.value = State(currentTab = Tab.WATER)
+    }
+
+    enum class Tab {
+        HISTORY,
+        INCOME_RATE,
+        WATER
     }
 }
