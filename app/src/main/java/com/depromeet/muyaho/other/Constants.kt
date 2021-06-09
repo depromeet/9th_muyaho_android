499a2d51943802ed60efd3f3cd6d93100ab2186e
SieunSong <se.song@riiid.co>
2021-06-10 00:37:58 +0900

    BaseUrl 변경

--- app/src/main/java/com/depromeet/muyaho/other/Constants.kt
+++ app/src/main/java/com/depromeet/muyaho/other/Constants.kt
@@ -1,7 +1,7 @@
 package com.depromeet.muyaho.other
 
 object Constants {
-    const val BASE_URL = "http://muyaho-api-alb-302156047.ap-northeast-2.elb.amazonaws.com/"
+    const val BASE_URL = "https://test.muyaho.xyz"
     const val DATABASE_NAME = "muyaho-db"
 
     const val TEST_SESSION_ID = "7b126a9c-3245-40a2-bfab-3f0ccb846ee3"
