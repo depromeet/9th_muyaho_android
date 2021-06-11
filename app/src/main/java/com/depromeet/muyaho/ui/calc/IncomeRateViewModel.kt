4d1b2041bef59e0c416d2408adc9b2ebf2610719
SieunSong <se.song@riiid.co>
2021-06-11 23:55:10 +0900

    수익금 계산 적용

--- app/src/main/java/com/depromeet/muyaho/ui/calc/IncomeRateViewModel.kt
+++ app/src/main/java/com/depromeet/muyaho/ui/calc/IncomeRateViewModel.kt
@@ -1,16 +1,13 @@
 package com.depromeet.muyaho.ui.calc
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.depromeet.muyaho.base.Action
 import com.depromeet.muyaho.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.math.ceil
 
 @HiltViewModel
 class IncomeRateViewModel @Inject constructor(
@@ -21,6 +18,14 @@ class IncomeRateViewModel @Inject constructor(
     val holdingPrice = MutableLiveData<String>()
     val holdingCount = MutableLiveData<String>()
     val purchaseAmount = MutableLiveData<String>()
+    val targetIncomeRate = MutableLiveData<String>()
+    private val _predictAmount = MutableLiveData<Double>()
+    val predictAmount: LiveData<String>
+        get() = _predictAmount
+            .map { it.toString() }
+            .map { it.substring(0 until it.length - 2) }
+    val visiblePredictView: LiveData<Boolean>
+        get() = predictAmount.map { it.isNotEmpty() }
 
     init {
         viewModelScope.launch {
@@ -30,10 +35,19 @@ class IncomeRateViewModel @Inject constructor(
                     val count = _count.toInt()
                     val price = _price.toInt()
                     count * price
-                }
-                .collect { amount: Int ->
+                }.collect { amount ->
                     if (amount != 0) purchaseAmount.value = amount.toString()
                 }
+
         }
+        // TODO 1000% 이하까지 구현
+        targetIncomeRate.asFlow()
+            .distinctUntilChanged()
+            .combine(purchaseAmount.asFlow()) { rate, amount ->
+                (amount.toDouble() * (rate.toDouble() / 100))
+            }
+            .onEach { total ->
+                _predictAmount.value = ceil(total)
+            }.launchIn(viewModelScope)
     }
 }
