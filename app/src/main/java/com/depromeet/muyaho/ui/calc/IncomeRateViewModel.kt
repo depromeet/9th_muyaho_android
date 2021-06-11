75b26893e88a75c63f525d7b4ed34cf7fe1594e8
SieunSong <se.song@riiid.co>
2021-06-11 17:44:17 +0900

    매입금액 나오도록 수정

--- app/src/main/java/com/depromeet/muyaho/ui/calc/IncomeRateViewModel.kt
+++ app/src/main/java/com/depromeet/muyaho/ui/calc/IncomeRateViewModel.kt
@@ -1,8 +1,15 @@
 package com.depromeet.muyaho.ui.calc
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
 import com.depromeet.muyaho.base.Action
 import com.depromeet.muyaho.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,7 +18,22 @@ class IncomeRateViewModel @Inject constructor(
 ) : BaseViewModel<IncomeRateViewModel.ViewAction>() {
     sealed class ViewAction : Action
 
-    init {
+    val holdingPrice = MutableLiveData<String>()
+    val holdingCount = MutableLiveData<String>()
+    val purchaseAmount = MutableLiveData<String>()
 
+    init {
+        viewModelScope.launch {
+            holdingCount.asFlow()
+                .distinctUntilChanged()
+                .combine(holdingPrice.asFlow()) { _count, _price ->
+                    val count = _count.toInt()
+                    val price = _price.toInt()
+                    count * price
+                }
+                .collect { amount: Int ->
+                    if (amount != 0) purchaseAmount.value = amount.toString()
+                }
+        }
     }
 }
